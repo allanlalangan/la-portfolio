2e6f0a4e4c989c764c06091c4a12a6ad2e8ad738
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-05-20 13:33:28 -0700

    update Skills page styles

--- components/Skills/Skills.jsx
+++ components/Skills/Skills.jsx
@@ -17,35 +17,86 @@ const Skills = () => {
   return (
     <section className={styles['skills-grid']}>
       <article className={styles['skills__primary']}>
-        <figure className={styles['icon-container']}>
-          <SiHtml5 className={styles.icon} />
-          <figcaption>html5</figcaption>
+        <figure className={styles['skill']}>
+          <SiReact className={styles['skill-icon']} />
+          <figcaption className={styles['skill-caption']}>
+            <h2 className={styles['_caption-title']}>React</h2>
+            <p className={styles['_caption-summary']}>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus
+              iusto facere omnis cum ratione doloremque tempora magni ducimus
+              voluptatibus explicabo!
+            </p>
+          </figcaption>
         </figure>
-        <figure className={styles['icon-container']}>
-          <SiCss3 className={styles.icon} />
-          <figcaption>css3</figcaption>
+      </article>
+      <article className={styles['skills__secondary']}>
+        <figure className={styles['skill']}>
+          <SiHtml5 className={styles['skill-icon']} />
+          <figcaption className={styles['skill-caption']}>
+            <h3 className={styles['_caption-title']}>html5</h3>
+            <p className={styles['_caption-summary']}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est atque
+              consequuntur excepturi ad veniam rerum officia non omnis qui
+              quibusdam.
+            </p>
+          </figcaption>
         </figure>
-        <figure className={styles['icon-container']}>
-          <SiJavascript className={styles.icon} />
-          <figcaption>javascript</figcaption>
+        <figure className={styles['skill']}>
+          <SiCss3 className={styles['skill-icon']} />
+          <figcaption className={styles['skill-caption']}>
+            <h3 className={styles['_caption-title']}>CSS3</h3>
+            <p className={styles['_caption-summary']}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est atque
+              consequuntur excepturi ad veniam rerum officia non omnis qui
+              quibusdam.
+            </p>
+          </figcaption>
         </figure>
-        <figure className={styles['icon-container']}>
-          <SiReact className={styles.icon} />
-          <figcaption>react</figcaption>
+        <figure className={styles['skill']}>
+          <SiJavascript className={styles['skill-icon']} />
+          <figcaption className={styles['skill-caption']}>
+            <h3 className={styles['_caption-title']}>Javascript</h3>
+            <p className={styles['_caption-summary']}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est atque
+              consequuntur excepturi ad veniam rerum officia non omnis qui
+              quibusdam.
+            </p>
+          </figcaption>
         </figure>
       </article>
       <article className={styles['skills__tools']}>
-        <figure className={styles['icon-container']}>
-          <SiNpm className={styles.icon} />
-          <figcaption>npm</figcaption>
+        <figure className={styles['skill']}>
+          <SiNpm className={styles['skill-icon']} />
+          <figcaption className={styles['skill-caption']}>
+            <h3 className={styles['_caption-title']}>NPM</h3>
+            <p className={styles['_caption-summary']}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est atque
+              consequuntur excepturi ad veniam rerum officia non omnis qui
+              quibusdam.
+            </p>
+          </figcaption>
         </figure>
-        <figure className={styles['icon-container']}>
-          <SiGithub className={styles.icon} />
-          <figcaption>github</figcaption>
+        <figure className={styles['skill']}>
+          <SiGithub className={styles['skill-icon']} />
+          <figcaption className={styles['skill-caption']}>
+            <h3 className={styles['_caption-title']}>GitHub</h3>
+            <p className={styles['_caption-summary']}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est atque
+              consequuntur excepturi ad veniam rerum officia non omnis qui
+              quibusdam.
+            </p>
+          </figcaption>
         </figure>
-        <div className={styles['icon-container']}>
-          <SiGooglemaps className={styles.icon} />
-          <figcaption>google maps api</figcaption>
+        <div className={styles['skill']}>
+          <SiGooglemaps className={styles['skill-icon']} />
+          <figcaption className={styles['skill-caption']}>
+            <h3 className={styles['_caption-title']}>Google Maps API</h3>
+            <p className={styles['_caption-summary']}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est atque
+              consequuntur excepturi ad veniam rerum officia non omnis qui
+              quibusdam.
+            </p>
+          </figcaption>
         </div>
       </article>
     </section>
